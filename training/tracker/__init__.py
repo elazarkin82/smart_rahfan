4814bca05d35a985de167362225fd7484e65ac12
elazarkin <elazarkin@gmail.com>
2026-05-29 09:50:58 +0300

    ddd

--- training/tracker/__init__.py
+++ training/tracker/__init__.py
@@ -1,5 +1,3 @@
-from .tracker_model import TargetTracker
-from .tracker_model2 import TargetTracker2
 from .tracker_ver3 import TargetTracker as TargetTracker3
 
-__all__ = ['TargetTracker', 'TargetTracker2', 'TargetTracker3']
+__all__ = ['TargetTracker3']
