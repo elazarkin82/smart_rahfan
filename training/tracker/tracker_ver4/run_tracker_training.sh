1aca80a3826df822a56a49b3624858c09f8244ca
elazarkin <elazarkin@gmail.com>
2026-05-31 19:01:57 +0300

    bbb

--- training/tracker/tracker_ver4/run_tracker_training.sh
+++ training/tracker/tracker_ver4/run_tracker_training.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # run_tracker_training.sh
+# dice_bce focal mse logcosh huber
 
 mkdir -p outputs
 
 python3 tracker_model.py train \
     --dataset_dir dataset_generator/dataset \
     --num_of_epochs 5 \
-    --lr 1e-4 \
-    --loss focal \
+    --lr 1e-3 \
+    --loss dice_bce \
     --batch_size 16 \
-    --eval_pkl_num 20 \
+    --eval_pkl_num 30 \
     --output outputs/tracker.keras \
     --init_keras_file outputs/tracker.keras \
     --best_train_loss_output outputs/tracker_best_train_loss.keras \
